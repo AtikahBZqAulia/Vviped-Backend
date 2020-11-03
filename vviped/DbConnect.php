352c58f271603338f09f7b1bf4befd11b8f1c017
octarinasalsa03 <octarinasalsabila@gmail.com>
2020-11-03 23:12:19 +0700

    update on DbConnect

--- vviped/DbConnect.php
+++ vviped/DbConnect.php
@@ -1,20 +1,20 @@
- <?php
-
-    //these are the server details
-    //the username is root by default in case of xampp
-    //password is nothing by default
-    //and lastly we have the database named android. if your database name is different you have to change it 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "vviped";
-
-
-    //creating a new connection object using mysqli 
-    $conn = new mysqli($servername, $username, $password, $database);
-
-    //if there is some error connecting to the database
-    //with die we will stop the further execution by displaying a message causing the error 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+<?php 
+ 
+//these are the server details
+//the username is root by default in case of xampp
+//password is nothing by default
+//and lastly we have the database named android. if your database name is different you have to change it 
+$servername = "localhost";
+$database = "jakart44_vvipeddb";
+$username = "jakart44_vvipeddb";
+$password = "pqJuM#tAdO0q";
+ 
+ 
+//creating a new connection object using mysqli 
+$conn = mysqli_connect($servername, $username, $password, $database);
+ 
+//if there is some error connecting to the database
+//with die we will stop the further execution by displaying a message causing the error 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
